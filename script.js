9a25c5071125cfd14a5fc85ef6aba7dc54b358ae
Anthony C <gitanthony@yahoo.com>
2022-04-21 19:33:23 -0400

    cookie msg

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+
+// Cookie msg
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+
+message.classList.add('cookie-message');
+message.innerHTML =
+    'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it!</button>';
+header.append(message);
+
+document
+    .querySelector('.btn--close-cookie')
+    .addEventListener('click', function () {
+        message.remove();
+    });
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+message.style.height =
+    Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
